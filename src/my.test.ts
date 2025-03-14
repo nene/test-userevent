7f561c2269d98abbb875d073db69cf3594a91cea
Rene Saarsoo <nene@triin.net>
2025-03-14 13:43:17 +0200

    Add failing usage of userEvent

--- src/my.test.ts
+++ src/my.test.ts
@@ -1,7 +1,10 @@
 import { describe, expect, it } from "vitest";
+import userEvent from "@testing-library/user-event";
 
 describe("my test", () => {
   it("should pass", () => {
+    console.log(userEvent.setup);
+    userEvent.setup();
     expect(true).toBe(true);
   });
 });
